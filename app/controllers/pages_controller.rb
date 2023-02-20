ca203510b38924a01f6893f0acf8834a54288587
Jp Fortuno <jp.fortuno@nullpod.com>
2023-02-20 11:03:56 +0400

    refine changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,5 +10,9 @@ class PagesController < ApplicationController
 
     def contact
         @members = ["jeremy", "ezekiel", "manuela", "havish", "jp"]
+
+        if params[:member]
+            @members = @members.select { |member| member.start_with?(params[:member]) }
+        end
     end
 end
